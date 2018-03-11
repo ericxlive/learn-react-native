7d0cd001d2cf05c65536e969814f3366fe1ebb54
Eric de Souza <ericdsky@gmail.com>
2018-03-10 21:25:54 -0500

    The very first component

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MyOwnComponent from './MyOwnComponent';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is my very first change.. it looks like it's easy right?</Text>
+        <MyOwnComponent/> 
       </View>
     );
   }
---
+++ MyOwnComponent.js
@@ -0,0 +1,11 @@
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class MyOwnComponent extends Component{
+    render(){
+        return (<Text>This is my very first change.. it looks like it's easy right?</Text>);
+    }
+}
+
+export default MyOwnComponent;
