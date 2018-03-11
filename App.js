26ab56c4fe9070ee29808f7df2bc1cc9c95bc6a0
Eric de Souza <ericdsky@gmail.com>
2018-03-10 19:16:58 -0500

    This is a pilot React Native project

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is my very first change... it looks like it's easy right?</Text>
+        <Text>This is my very first change.. it looks like it's easy right?</Text>
       </View>
     );
   }
